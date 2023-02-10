9f4634af1dd5e215fe4f2ccfd15150d2996d64a8
Thiago Rocha <Thiagorochakl@gmail.com>
2023-02-09 23:35:22 -0300

    Adding new reducer on store

--- src/store.jsx
+++ src/store.jsx
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { TierListLineReducer } from "./features/reduxTierListLine";
+import { ListItemsReducer } from "./features/reduxListItems";
 
 export const store = configureStore({
     reducer: {
-        tierListLine: TierListLineReducer
+        tierListLine: TierListLineReducer,
+        listItems: ListItemsReducer
     }
 })
