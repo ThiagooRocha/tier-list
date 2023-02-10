7363103a19a8a39e802c8d61019963db710f3998
Thiago Rocha <Thiagorochakl@gmail.com>
2023-02-09 23:37:24 -0300

    Updating componets with reducers

--- src/components/listItems/FormAddItem.jsx
+++ src/components/listItems/FormAddItem.jsx
@@ -1,40 +1,29 @@
 import React, { useState } from "react";
 
-export function FormAddItem({ imgs, setImgs }) {
+//Reducers
+import { useSelector, useDispatch } from "react-redux";
+import { Add_New_Item } from "../../features/reduxListItems"
+
+export function FormAddItem() {
+  const listItems = useSelector(state => state.listItems)
+  const dispatch = useDispatch()
   const [inputUrl, setInputUrl] = useState("");
-  const [inputFile, setInputFile] = useState();
-
-  function convertFileToImage(e) {
-    
-    
-  }
-
-  function convertUrl() {
-    if (inputUrl !== "") {
-      try {
-        new URL(inputUrl);
-        setImgs((prev) => [...prev, inputUrl])
-      } catch (error) {
-        alert("URL inválida");
-      }
-      setInputUrl("");
-    }
-  }
 
   function handleSubmit(e) {
     e.preventDefault();
-
-    convertUrl();
-
-    if (inputFile) {
-      convertFileToImage();
+    if(inputUrl !== ""){
+      dispatch(Add_New_Item({inputUrl}))
+      setInputUrl("");
     }
   }
 
   return (
     <form className="flex flex-col gap-3" onSubmit={handleSubmit}>
+
       <div className="flex flex-col">
-        <label htmlFor="url" className="text-zinc-300">Url</label>
+        <label htmlFor="url" className="text-zinc-300">
+          Url
+        </label>
         <input
           className="w-full bg-zinc-400 rounded-md h-10 p-4 text-zinc-800 placeholder:text-zinc-600 outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-1 focus:ring-offset-zinc-300"
           type="text"
@@ -44,21 +33,6 @@ export function FormAddItem({ imgs, setImgs }) {
           value={inputUrl}
           onChange={(e) => setInputUrl(e.target.value)}
         />
-      </div>
-
-      <div className="flex flex-col">
-        <label htmlFor="file" className="text-zinc-300">File</label>
-        <div>
-          <input
-            type="file"
-            id="file"
-            className="w-full bg-zinc-400 rounded-md h-10 text-zinc-700 flex items-center justify-center placeholder:text-zinc-600 outline-none focus:ring-2 focus:ring-zinc-400 focus:ring-offset-1 focus:ring-offset-zinc-300"
-            accept="image/*"
-            onChange={convertFileToImage}
-          />
-        </div>
-
-        
       </div>
 
       <button
--- src/components/listItems/ListItems.jsx
+++ src/components/listItems/ListItems.jsx
@@ -1,25 +1,57 @@
-import { useState } from "react";
+
+//Components
 import { ModalAddImage } from "./ModalAddImage";
 
+//Drag and Drop
+import { Droppable, Draggable } from "react-beautiful-dnd";
+
+//Reducers
+import { useSelector } from "react-redux";
+
 export function ListItems() {
-  const [imgs, setImgs] = useState([]);
+
+  const listItems = useSelector(state => state.listItems)
 
   return (
     <div className="bg-zinc-300 rounded-md w-full max-h-[320px] overflow-auto flex gap-2">
       <div className="w-36 p-3">
-        <ModalAddImage imgs={imgs} setImgs={setImgs} />
+        <ModalAddImage />
       </div>
 
-      <div className="w-full flex flex-wrap">
-        {imgs &&
-          imgs.map((img, i) => (
-            <img
-              key={img + i}
-              className="w-[150px] h-[80px] md:h-40 object-contain"
-              src={img}
-            />
-          ))}
-      </div>
+        <Droppable  droppableId={"listImgs"}>
+          {(provided) => (
+            <div
+              className="w-full flex flex-wrap"
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+            >
+              {listItems &&
+                listItems.map((img, index) => (
+                  <Draggable
+                    key={img.id}
+                    draggableId={img.id}
+                    index={index}
+                  >
+                    {(provided) => (
+                      <div
+                        className="flex-shrink-0 w-full h-40 sm:w-32 sm:h-32"
+                        ref={provided.innerRef}
+                        {...provided.dragHandleProps}
+                        {...provided.draggableProps}
+                        style={{
+                          backgroundImage: `url(${img.src})`,
+                          backgroundPosition: "center",
+                          backgroundSize: "cover",
+                          ...provided.draggableProps.style,
+                        }}
+                      />
+                    )}
+                  </Draggable>
+                ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
     </div>
   );
 }
